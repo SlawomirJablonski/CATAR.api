8c47904531cb3fb2a1f98440bfb013b9ac1e5071
SlawomirJablonski <forest19@interia.pl>
2019-05-11 12:59:06 +0200

    first commit

---
+++ src/main/java/com/restapp/catar/CatarApplication.java
@@ -0,0 +1,13 @@
+package com.restapp.catar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/restapp/catar/CatarApplicationTests.java
@@ -0,0 +1,16 @@
+package com.restapp.catar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CatarApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
